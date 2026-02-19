3d6266e8d00af9b92dcd74a58b515b7ee4b943df
savioros <saviopeck@outlook.com>
2026-02-19 20:39:39 -0300

    Endpoint route, controller and request created

---
+++ app/Http/Controllers/RestaurantController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RestaurantController extends Controller
+{
+    public function store()
+    {
+        
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RestaurantController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,4 @@ Route::get('/', function () {
 
 Route::post('/auth/register', [UserController::class, 'store']);
 Route::post('/auth/login', [UserController::class, 'login']);
+Route::post('/restaurants', [RestaurantController::class, 'store']);
