7ba948f51a77754c870e2f857fbd75cac555b808
savioros <saviopeck@outlook.com>
2026-02-19 22:39:37 -0300

    Get all the restaurants

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -6,10 +6,16 @@ use App\Exceptions\CreateRestaurantException;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RestaurantRegisterRequest;
 use App\Http\Resources\RestaurantResource;
+use App\Models\Restaurant;
 use App\Services\RegisterRestaurantService;
 
 class RestaurantController extends Controller
 {
+    public function index()
+    {
+        return RestaurantResource::collection(Restaurant::all());    
+    }
+
     public function store(RestaurantRegisterRequest $request, RegisterRestaurantService $service)
     {
         try {
