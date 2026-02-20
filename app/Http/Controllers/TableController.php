c4986929fb69816fb901b7937ffbafe092b0af3b
savioros <saviopeck@outlook.com>
2026-02-20 11:57:04 -0300

    Creating the method to create the table and the custom exception

--- app/Http/Controllers/TableController.php
+++ app/Http/Controllers/TableController.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\CreateTableException;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\TableRegisterRequest;
+use App\Models\Restaurant;
+use App\Services\RegisterTableService;
 
 class TableController extends Controller
 {
-    public function store()
+    public function store(TableRegisterRequest $request, Restaurant $restaurant, RegisterTableService $service)
     {
-        
+        try {
+            $table = $service->create($restaurant->id, $request->validated());
+            return response()->json($table);
+        } catch (CreateTableException $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ]);
+        }
     }
 }
