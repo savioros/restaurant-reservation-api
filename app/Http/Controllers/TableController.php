516a918bfd096d4adc5205972a7f8acb33af6e9d
savioros <saviopeck@outlook.com>
2026-02-24 07:31:59 -0300

    Rule to verify if the user is the restaurant owner

--- app/Http/Controllers/TableController.php
+++ app/Http/Controllers/TableController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Exceptions\CreateTableException;
+use App\Exceptions\UserAreProhibitedCreateOrModifyingThirdpartyRestaurant;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TableRegisterRequest;
 use App\Http\Resources\TableResource;
@@ -21,12 +22,16 @@ class TableController extends Controller
     public function store(TableRegisterRequest $request, Restaurant $restaurant, RegisterTableService $service)
     {
         try {
-            $table = $service->create($restaurant->id, $request->validated());
+            $table = $service->create(auth()->id(), $restaurant, $request->validated());
             return new TableResource($table);
         } catch (CreateTableException $e) {
             return response()->json([
                 'message' => $e->getMessage()
-            ]);
+            ], 500);
+        } catch (UserAreProhibitedCreateOrModifyingThirdpartyRestaurant $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 403);
         }
     }
 }
