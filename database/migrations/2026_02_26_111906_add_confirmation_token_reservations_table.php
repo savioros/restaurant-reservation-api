f56a1c9ddc467edc2dd9725ce834200d455dbc90
savioros <saviopeck@outlook.com>
2026-02-26 08:21:51 -0300

    Create confirmation token

---
+++ database/migrations/2026_02_26_111906_add_confirmation_token_reservations_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('reservations', function (Blueprint $table) {
+            $table->string('confirmation_token', 64)->unique()->nullable();
+            $table->timestamp('confirmation_expires_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('reservations', function (Blueprint $table) {
+            $table->dropColumn('confirmation_token');
+            $table->dropColumn('confirmation_expires_at');
+        });
+    }
+};
