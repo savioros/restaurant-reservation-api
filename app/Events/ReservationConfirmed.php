eb87fbff25f34e94097751c0b0643c667a676e2a
savioros <saviopeck@outlook.com>
2026-02-26 19:13:41 -0300

    Create workflow for reservation confirmation with event and listener

---
+++ app/Events/ReservationConfirmed.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ReservationConfirmed
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(
+        public readonly string $customer_name,
+        public readonly string $reservation_date,
+        public readonly string $start_time,
+        public readonly string $end_time,
+        public readonly string $restaurant,
+        public readonly int $table,
+        public readonly int $guests_count,
+        public readonly string $customer_email
+    )
+    {}
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('channel-name'),
+        ];
+    }
+}
